c552a40b0621790793353fe0ee682bbffe59f83b
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-01-23 17:08:52 +0100

    Add plots

--- src/client/client.py
+++ src/client/client.py
@@ -12,11 +12,12 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     conn = rpyc.connect("rpi32.local", 18861, config={"allow_pickle": True})
-    sample_rate, data = conn.root.run_adc_sample()
+    sample_period, data = conn.root.run_adc_sample()
 
     # Make a local copy of the data
     local_data = np.array(data)
-    print(f"Sample rate: {sample_rate}")
+    local_sample_period = float(sample_period)
+    print(f"Sample rate: {sample_period}")
 
     conn.close()
 
@@ -24,9 +25,33 @@ if __name__ == "__main__":
     print(f"Data type: {type(local_data)}")
     print(f"Data shape: {local_data.shape}")
     local_data = local_data.T
-    for i, channel in enumerate(local_data):
-        plt.plot(channel, label=f"Channel {i}")
 
+    t = np.linspace(0, local_sample_period * local_data.shape[1], local_data.shape[1])
+    t_ms = 1e3 * t
+    fig, ax = plt.subplots(5, 1)
+    for i, data_channel in enumerate(local_data):
+
+        data_voltage = data_channel * (3.3 / (2**12 - 1))
+        ax[i].plot(t_ms, data_voltage, label=f"Channel {i+1}")
+        ax[i].set_xlabel("Time (ms)")
+        ax[i].set_ylabel("Voltage (V)")
+        ax[i].grid()
+        ax[i].legend()
+
+    # Do FFT of signals
+    plt.figure()
+
+    for i, data_channel in enumerate(local_data):
+        data_voltage = data_channel * (3.3 / (2**12 - 1))
+
+        freqs = np.fft.fftshift(np.fft.fftfreq(len(data_voltage), local_sample_period))
+        fft = np.fft.fftshift(np.fft.fft(data_voltage))
+        fft = np.abs(fft)
+        plt.plot(freqs, fft, label=f"Channel {i+1}")
+
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Magnitude")
     plt.grid()
     plt.legend()
+
     plt.show()
