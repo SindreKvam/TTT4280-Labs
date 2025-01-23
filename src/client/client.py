cbf93a58208558abfbfa3e775932335c8db98efa
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-01-23 12:36:40 +0100

    Successfully read and plot data

--- src/client/client.py
+++ src/client/client.py
@@ -3,12 +3,30 @@
 import logging
 
 import rpyc
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
-    conn = rpyc.connect("rpi32.local", 18861)
-    print(conn.root.run_adc_sample())
+    conn = rpyc.connect("rpi32.local", 18861, config={"allow_pickle": True})
+    sample_rate, data = conn.root.run_adc_sample()
+
+    # Make a local copy of the data
+    local_data = np.array(data)
+    print(f"Sample rate: {sample_rate}")
+
     conn.close()
+
+    print(f"Data: {local_data}")
+    print(f"Data type: {type(local_data)}")
+    print(f"Data shape: {local_data.shape}")
+    local_data = local_data.T
+    for i, channel in enumerate(local_data):
+        plt.plot(channel, label=f"Channel {i}")
+
+    plt.grid()
+    plt.legend()
+    plt.show()
