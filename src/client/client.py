d99ce9cb75b5cf4514bb83e82638929afce087bd
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-01-24 23:01:46 +0100

    Added dc_block functionality

--- src/client/client.py
+++ src/client/client.py
@@ -25,12 +25,19 @@ def sample_value_to_voltage(value: int) -> float:
     return value * (V_REF / (2**ADC_RESOLUTION - 1))
 
 
+def dc_block(data: np.ndarray) -> np.ndarray:
+    """Remove DC offset from the data."""
+    return data - np.mean(data)
+
+
 if __name__ == "__main__":
     conn = rpyc.connect("rpi32.local", 18861, config={"allow_pickle": True})
     sample_period, data = conn.root.run_adc_sample()
 
     # Make a local copy of the data
     local_data = np.array(data)
+    # Remove the first few samples as the start gives random values.
+    local_data = local_data[10:]
     local_sample_period = float(sample_period)
     print(f"Sample rate: {sample_period}")
 
@@ -41,26 +48,40 @@ if __name__ == "__main__":
     print(f"Data shape: {local_data.shape}")
     local_data = local_data.T
 
-    t = np.linspace(0, local_sample_period * local_data.shape[1], local_data.shape[1])
-    t_ms = 1e3 * t
-    fig, ax = plt.subplots(5, 1)
-    for i, data_channel in enumerate(local_data):
-        data_voltage = sample_value_to_voltage(data_channel)
+    print(f"Data: {local_data}")
 
-        ax[i].plot(t_ms, data_voltage, label=f"Channel {i+1}")
-        ax[i].set_xlabel("Time (ms)")
+    # Turn the data into voltages and remove DC offset.
+    data_voltage = []
+    for i, data_channel in enumerate(local_data):
+        data_voltage.append(sample_value_to_voltage(data_channel))
+        data_voltage[i] = dc_block(data_voltage[i])
+
+    data_voltage = np.array(data_voltage)
+
+    # Plot the data
+    t_ms = (
+        np.linspace(0, local_sample_period * local_data.shape[1], local_data.shape[1])
+        * 1e3
+    )
+    fig, ax = plt.subplots(5, 1, tight_layout=True, sharex=True)
+    for i, data_channel in enumerate(data_voltage):
+
+        ax[i].plot(t_ms, data_channel, f"C{i}", label=f"Channel {i+1}")
         ax[i].set_ylabel("Voltage (V)")
         ax[i].grid()
         ax[i].legend()
+        ax[i].set_ylim(-V_REF / 2, V_REF / 2)
+
+    ax[-1].set_xlabel("Time (ms)")
+
+    fig.suptitle("ADC Data")
 
     # Do FFT of signals
     plt.figure()
 
-    for i, data_channel in enumerate(local_data):
-        data_voltage = sample_value_to_voltage(data_channel)
-
-        freqs = np.fft.fftshift(np.fft.fftfreq(len(data_voltage), local_sample_period))
-        fft = np.fft.fftshift(np.fft.fft(data_voltage))
+    for i, data_channel in enumerate(data_voltage):
+        freqs = np.fft.fftshift(np.fft.fftfreq(len(data_channel), local_sample_period))
+        fft = np.fft.fftshift(np.fft.fft(data_channel))
         fft = np.abs(fft)
         plt.plot(freqs, fft, label=f"Channel {i+1}")
 
