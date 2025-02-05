e71f76af4746e417b84947b41a18dc4b98c4a0c2
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-02-05 19:08:01 +0100

    Added commandline arguments to client.py

--- src/client/client.py
+++ src/client/client.py
@@ -2,8 +2,12 @@
 
 import logging
 import time
-
-from common import sample_value_to_voltage, dc_block, V_REF
+import argparse
+from common import (
+    sample_value_to_voltage,
+    dc_block,
+    V_REF,
+)  # pylint: disable=import-error
 
 import rpyc
 import numpy as np
@@ -13,12 +17,10 @@ import dwfpy as dwf
 logger = logging.getLogger(__name__)
 
 
-def run_measurement_and_save_to_file(
-    pi_connection, filename: str = "adc_data.npz"
-) -> None:
+def run_measurement(pi_connection, num_samples: int = 31_260) -> None:
     """Run a measurement on the Raspberry Pi and save the data to a file."""
 
-    sample_period, data = pi_connection.root.run_adc_sample(31_260)
+    sample_period, data = pi_connection.root.run_adc_sample(num_samples)
 
     # Make a local copy of the data
     local_data = np.array(data)
@@ -42,30 +44,74 @@ def run_measurement_and_save_to_file(
 
     data_voltage = np.array(data_voltage)
 
-    # Save the data to a file that can be used for later analysis
+    return data_voltage, local_sample_period
+
+
+def save_data_to_file(
+    data: np.ndarray,
+    sample_period: float,
+    *,
+    filename: str = "adc_data.npz",
+    output_dir: str = "output_data",
+) -> None:
+    """Save the data to a file that can be used for later analysis."""
     np.savez(
-        f"output_data/{filename}",
-        sample_period=np.float64(local_sample_period),
-        data=data_voltage,
+        f"{output_dir}/{filename}",
+        sample_period=np.float64(sample_period),
+        data=data,
     )
 
 
 if __name__ == "__main__":
-    conn = rpyc.connect("rpi32.local", 18861, config={"allow_pickle": True})
 
-    run_measurement_and_save_to_file(conn)
+    parser = argparse.ArgumentParser(description="Run a measurement on the Raspberry Pi.")
+    parser.add_argument(
+        "-i",
+        "--ip",
+        type=str,
+        help="The IP address of the Raspberry Pi.",
+        default="rpi32.local",
+    )
+    parser.add_argument(
+        "-s", "--frequency_sweep", action="store_true", help="Run a frequency sweep."
+    )
+    parser.add_argument(
+        "-o", "--output", type=str, help="The output filename.", default="adc_data.npz"
+    )
+    parser.add_argument(
+        "--output-dir", type=str, help="The output directory.", default="output_data"
+    )
+    parser.add_argument(
+        "-n",
+        "--num_samples",
+        type=int,
+        help="The number of samples to collect.",
+        default=31_260,
+    )
+    args = parser.parse_args()
 
-    # Connect to the Analog Discovery 2
-    with dwf.Device() as device:
+    # Connect to the instrument
+    conn = rpyc.connect(args.ip, 18861, config={"allow_pickle": True})
 
-        for f in np.logspace(1, 6, 100):
+    # Run measurements
+    data, sample_period = run_measurement(conn, num_samples=args.num_samples)
+    save_data_to_file(
+        data, sample_period, filename=args.output, output_dir=args.output_dir
+    )
 
-            print(f"Generating a {f:.2f}Hz sine wave on WaveGen channel 1...")
-            device.analog_output["ch1"].setup(
-                "sine", frequency=f, amplitude=V_REF / 2, offset=V_REF / 2, start=True
-            )
-            time.sleep(0.2)
+    if args.frequency_sweep:
+        # Connect to the Analog Discovery 2 and run frequency sweep
+        with dwf.Device() as device:
 
-            run_measurement_and_save_to_file(conn, filename=f"adc_data_{f:.2f}Hz.npz")
+            for f in np.logspace(1, 6, 100):
+
+                print(f"Generating a {f:.2f}Hz sine wave on WaveGen channel 1...")
+                device.analog_output["ch1"].setup(
+                    "sine", frequency=f, amplitude=V_REF / 2, offset=V_REF / 2, start=True
+                )
+                time.sleep(0.2)
+
+                data, sample_period = run_measurement(conn)
+                save_data_to_file(data, sample_period, filename=f"adc_data_{f:.2f}Hz.npz")
 
     conn.close()
